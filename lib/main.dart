08bdb714eddb4dcb9d65d68ffbf9415194bc6eb8
Anjali Jain <anjali.jain@mykaarma.com>
2023-03-26 02:41:56 +0530

    2023-03-26 blog6

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void selectOption() {
+    print("selected an option");
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ["What is your first name?", "What is your last name?"];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('Cadbury'),
       ),
-      body: Text('This is the home screen'),
+      body: Column(
+        children: [
+          Text("question 1"),
+          ElevatedButton(
+            onPressed: () => print("chosen option 1"),
+            child: Text("Option 1"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print("chosen option 2");
+            },
+            child: Text("Option 2"),
+          ),
+          ElevatedButton(
+            onPressed: selectOption,
+            child: Text("Option 3"),
+          ),
+        ],
+      ),
     ));
   }
 }
